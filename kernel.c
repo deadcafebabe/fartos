dcfef1b304462ec22f1c22c6847daf23203bf1b0
64packsofstraws <64packsofstraws@users.noreply.github.com>
2026-05-28 19:26:07 +0300

    Add row and column variables to keep track of the cursor

--- kernel.c
+++ kernel.c
@@ -1,16 +1,42 @@
-void puts(char* str)
-{
-    static char* video_mem = (char*)0xB8000;
+#include <stdint.h>
 
+#define VIDEO_HEIGHT 25
+#define VIDEO_WIDTH 80
+#define VIDEO_MEMORY 0xB8000
+
+uint8_t column;
+uint8_t row;
+uint16_t* video_mem;
+
+void terminal_init()
+{
+    column = 0;
+    row = 0;
+    video_mem = (uint16_t*)VIDEO_MEMORY;
+}
+
+uint16_t vga_entry(uint8_t chr, uint8_t clr)
+{
+    return clr << 8 | chr;
+}
+
+void puts(uint8_t* str)
+{
     while (*str) {
-        *video_mem = *str;
-        *(video_mem + 1) = 0x7;
-        video_mem += 2;
+        *video_mem = vga_entry(*str, 0x7);
         str++;
+        video_mem++;
+        row++;
+
+        if (row == VIDEO_WIDTH) {
+            row = 0;
+            column++;
+        }
     }
 }
 
 void main()
 {
+    terminal_init();
     puts("hello world");
 }
